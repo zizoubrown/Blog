c8cedf4a8c20f6f6539c48f89f7238b3237d2ebd
Zizoubrown <zizumoha2015@gmail.com>
2020-07-22 23:19:53 +0300

    add register routes

--- flaskbog/routes.py
+++ flaskbog/routes.py
@@ -15,3 +15,18 @@ def home():
     quote = request.get_quote()
     posts = Post.query.order_by(Post.date_posted.desc()).paginate( per_page=5)
     return render_template('home.html', posts=posts, quote=quote)
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
