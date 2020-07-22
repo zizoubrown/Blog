d72bb9e79f2381bbe7e5f2849848672711d48a48
Zizoubrown <zizumoha2015@gmail.com>
2020-07-22 23:45:18 +0300

    add comment class

--- flaskbog/models.py
+++ flaskbog/models.py
@@ -29,3 +29,12 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    def __repr__(self):
+        return f"Comment('{self.date_posted}')"
+
