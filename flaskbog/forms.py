c17263eddb1408361ebcd0e83eb1e23d55008666
Zizoubrown <zizumoha2015@gmail.com>
2020-07-22 23:54:06 +0300

    add registration form

--- flaskbog/forms.py
+++ flaskbog/forms.py
@@ -4,3 +4,25 @@ from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=3, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign up')
+
+    def validate_username(self, username):
+
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already taken. Please create a new username.')
+
+
+    def validate_email(self, email):
+
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('email already taken. Please create a new email.')
